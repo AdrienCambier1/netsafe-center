8019ca1cc3e2bdf29921ba90ee7343a5a36b314d
Cambier-Adrien <adrien.cambier04@gmail.com>
2024-11-05 15:32:10 +0100

    05/11/2024

--- src/Components/Backgrounds/Modalbackground.jsx
+++ src/Components/Backgrounds/Modalbackground.jsx
@@ -10,7 +10,7 @@ export default function ModalBackground({ isOpen, onClick }) {
       onClick={onClick}
       className={`${
         isOpen ? "block" : "hidden"
-      } w-full h-full text-yellow-50 fixed top-0 right-0 bg-black/50`}
+      } w-full h-full fixed top-0 right-0 bg-black/50`}
     ></div>
   );
 }
