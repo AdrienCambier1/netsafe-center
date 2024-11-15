de4f8568b3059cfa35f0cfe2235dbc3bc61ede1b
Cambier-Adrien <adrien.cambier04@gmail.com>
2024-11-15 21:05:23 +0100

    15/11/2024

--- src/Components/Titles/ThirdTitle.jsx
+++ src/Components/Titles/ThirdTitle.jsx
@@ -1,7 +1,7 @@
 export default function ThirdTitle({ value }) {
   return (
     <div className="flex items-center relative h-fit">
-      <div className="bg-purple-500 dark:bg-purple-700 shadow-md dark:shadow-purple-700/50 shadow-purple-500/50 h-6 w-2 rounded-md mr-3"></div>
+      <div className="bg-purple-500 shadow-md shadow-purple-500/50 h-6 w-2 rounded-md mr-3"></div>
       <h3 className="text-xl text-zinc-800 dark:text-zinc-200 font-['Raleway'] font-medium">
         {value}
       </h3>
