43f65d3b575dd1787dcd320a246b2abf295752b5
Cambier-Adrien <adrien.cambier04@gmail.com>
2024-12-09 16:08:20 +0100

    {~} Prevent rendering of pages that require a connection

--- src/Page/Main.jsx
+++ src/Page/Main.jsx
@@ -27,11 +27,12 @@ import {
 } from "./Routes/AccountCenterContent";
 import { Question } from "./Routes/QuizContent";
 import { ClassSection } from "./Routes/ClassContent";
-import { ThemeContext } from "../Contexts";
+import { ConnectionContext, ThemeContext } from "../Contexts";
 import { useContext } from "react";
 
 export default function Main() {
   const { theme } = useContext(ThemeContext);
+  const { connection } = useContext(ConnectionContext);
 
   return (
     <main
@@ -44,7 +45,12 @@ export default function Main() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" />} />
-        <Route path="account_center" element={<AccountCenter />}>
+        <Route
+          path="account_center"
+          element={
+            connection === true ? <AccountCenter /> : <Navigate to="/" />
+          }
+        >
           <Route path="account_overview" element={<AccountOverview />} />
           <Route path="personal_data" element={<PersonalData />} />
           <Route
@@ -53,20 +59,38 @@ export default function Main() {
           />
           <Route path="appearance" element={<Appeareance />} />
         </Route>
-        <Route path="classes_list" element={<ClassesList />} />
-        <Route path="quiz_list" element={<QuizList />} />
+        <Route
+          path="classes_list"
+          element={connection === true ? <ClassesList /> : <Navigate to="/" />}
+        />
+        <Route
+          path="quiz_list"
+          element={connection === true ? <QuizList /> : <Navigate to="/" />}
+        />
         <Route path="useful_tools" element={<UsefulTools />} />
         <Route path="knowledge_area" element={<KnowledgeArea />} />
         <Route path="forum" element={<Forum />}>
           <Route path="recent_posts" element={<RecentPosts />} />
           <Route path="most_liked_posts" element={<MostLikedPosts />} />
-          <Route path="saved_posts" element={<SavedPosts />} />
-          <Route path="user_posts" element={<UserPosts />} />
+          <Route
+            path="saved_posts"
+            element={connection === true ? <SavedPosts /> : <Navigate to="/" />}
+          />
+          <Route
+            path="user_posts"
+            element={connection === true ? <UserPosts /> : <Navigate to="/" />}
+          />
         </Route>
-        <Route path="quiz/:quizId" element={<Quiz />}>
+        <Route
+          path="quiz/:quizId"
+          element={connection === true ? <Quiz /> : <Navigate to="/" />}
+        >
           <Route path="" element={<Question />} />
         </Route>
-        <Route path="class/:classId" element={<Class />}>
+        <Route
+          path="class/:classId"
+          element={connection === true ? <Class /> : <Navigate to="/" />}
+        >
           <Route path="section/:sectionId" element={<ClassSection />} />
         </Route>
       </Routes>
