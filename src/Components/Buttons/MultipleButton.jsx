5f6c8d5bd18d8e78c5e55dcb53a264b0826cd551
Cambier-Adrien <adrien.cambier04@gmail.com>
2024-12-16 13:51:11 +0100

    {~} Modified MultipleButton to former style

--- src/Components/Buttons/MultipleButton.jsx
+++ src/Components/Buttons/MultipleButton.jsx
@@ -8,15 +8,11 @@ export default function MultipleButton({ buttons, customPosition }) {
       <div className="flex flex-col p-2 rounded-xl bg-neutral-100 dark:bg-neutral-900">
         {buttons.map((button) => (
           <Link
-            className={`${
-              button.isDangerous
-                ? "hover:bg-red-500 dark:hover:bg-red-700 hover:shadow-red-500/50 dark:hover:shadow-red-700/50"
-                : null
-            } shadow-md shadow-transparent group flex gap-4 items-center justify-between rounded-md p-2`}
+            className="group flex gap-4 items-center justify-between rounded-md p-2"
             onClick={button.onClick}
           >
             {button.value && button.isDangerous ? (
-              <p className="text-sm font-['Raleway'] font-medium text-red-500 group-hover:text-white">
+              <p className="text-sm font-['Raleway'] font-medium text-red-500">
                 {button.value}
               </p>
             ) : (
@@ -26,7 +22,7 @@ export default function MultipleButton({ buttons, customPosition }) {
               <FontAwesomeIcon
                 className={`h-4 w-4 ${
                   button.isDangerous
-                    ? "text-red-300 dark:text-red-700 group-hover:text-white"
+                    ? "text-red-300 dark:text-red-700 group-hover:text-red-500"
                     : "text-neutral-300 group-hover:text-neutral-600 dark:text-neutral-700 dark:group-hover:text-neutral-400"
                 }`}
                 icon={button.icon}
