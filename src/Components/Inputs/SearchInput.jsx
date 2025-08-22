0bd3bb9b443f7ca182ba05213e12ef6b111ab49a
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-08-22 22:20:11 +0200

    Refactor SearchInput component: change input type to search and enhance accessibility attributes

--- src/Components/Inputs/SearchInput.jsx
+++ src/Components/Inputs/SearchInput.jsx
@@ -7,12 +7,14 @@ export default function SearchInput({ placeholder, onClick, value, onChange }) {
     <div className="group search-input">
       <FontAwesomeIcon className="gray-icon" icon={faSearch} />
       <input
-        type="text"
+        type="search"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
         autoComplete="off"
         name={`search_${Date.now()}`}
+        role="searchbox"
+        data-form-type="search"
       />
       <IconPurpleButton
         icon={faPlus}
